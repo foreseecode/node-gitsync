013af3517d1d2966bd4c7af8eff1a71c806be2ea
gpeu <Guillaume.Peugniez@foresee.com>
2018-11-02 08:52:47 -0700

    Use mkdirSync to throw any exception

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * Dependencies
  */
-var fs = require('fs'),
-  rimraf = require('rimraf'),
-  colors = require('colors'),
-  simpleGit = require('./lib/simple-git/index');
+var fs = require("fs"),
+  rimraf = require("rimraf"),
+  colors = require("colors"),
+  simpleGit = require("./lib/simple-git/index");
 
 /**
  * Is the dir empty?
@@ -12,10 +12,10 @@ var fs = require('fs'),
  * @param cb
  */
 function isDirEmpty(dirname, cb) {
-  fs.readdir(dirname, function (err, files) {
+  fs.readdir(dirname, function(err, files) {
     if (err) {
       // some sort of error
-      cb({message: "error"}, true);
+      cb({ message: "error" }, true);
     } else {
       if (!files.length) {
         // directory appears to be empty
@@ -32,7 +32,7 @@ function isDirEmpty(dirname, cb) {
  * @constructor
  */
 
-var GITSync = function (obj, cb) {
+var GITSync = function(obj, cb) {
   var loc = obj.dest;
   if (!obj.dest) {
     throw new Error("Destination (dest) folder is required.");
@@ -47,53 +47,59 @@ var GITSync = function (obj, cb) {
   }
 
   // Make sure there is a callback
-  cb = cb || function () {
+  cb =
+    cb ||
+    function() {
       console.log("Git Sync finished.");
     };
 
   // Decide where this goes
-  var fullqualifiedplace = obj.dest + '/' + obj.branch,
+  var fullqualifiedplace = obj.dest + "/" + obj.branch,
     needToSync = false;
 
-  fullqualifiedplace = fullqualifiedplace.replace(/\/\//g, '/');
+  fullqualifiedplace = fullqualifiedplace.replace(/\/\//g, "/");
 
   // Check if we need to make the parent folder
   if (!fs.existsSync(obj.dest)) {
-    fs.mkdir(obj.dest);
+    fs.mkdirSync(obj.dest);
     needToSync = true;
   }
   if (!fs.existsSync(fullqualifiedplace)) {
-    fs.mkdir(fullqualifiedplace);
+    fs.mkdirSync(fullqualifiedplace);
     needToSync = true;
   }
 
-  isDirEmpty(fullqualifiedplace, function (err, isempty) {
+  isDirEmpty(fullqualifiedplace, function(err, isempty) {
     if (isempty || needToSync) {
       var client = new simpleGit(obj.dest);
-      console.info("gitsync".yellow + ": ".grey + "Wait a moment, pulling repo " + obj.repo.magenta + " branch " + obj.branch.magenta + " to " + fullqualifiedplace.magenta + "...");
+      console.info(
+        "gitsync".yellow +
+          ": ".grey +
+          "Wait a moment, pulling repo " +
+          obj.repo.magenta +
+          " branch " +
+          obj.branch.magenta +
+          " to " +
+          fullqualifiedplace.magenta +
+          "..."
+      );
 
-      client.raw(
-        [
-          'clone',
-          '-b',
-          obj.branch,
-          obj.repo,
-          obj.branch
-        ], function(err, result) {
-          if (err) {
-            rimraf(fullqualifiedplace, function() {
-              cb(err);
-            });
-          } else {
-            cb();
-          }
-        });
-
+      client.raw(["clone", "-b", obj.branch, obj.repo, obj.branch], function(
+        err,
+        result
+      ) {
+        if (err) {
+          rimraf(fullqualifiedplace, function() {
+            cb(err);
+          });
+        } else {
+          cb();
+        }
+      });
     } else {
       cb();
     }
   });
-
 };
 
 /**
